add6936195c837d3414848a6781ec3eb806408a6
Tolsee <tolsee3@gmail.com>
2019-02-05 22:00:39 +0545

    Basic components with little bit of styling

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,25 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
-  templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.sass']
+  template: `
+    <ng-content></ng-content>
+  `,
+  styles: [`
+    :host {
+      display: flex;
+    }
+  `]
 })
 export class GridComponent implements OnInit {
   @Input() direction: string;
+  @Input() justify: string;
   @Input() align: string;
+  @Input() width: string;
+  @Input() passClass: string;
+
+  @HostBinding('style.flex-direction') get flexDirection() {
+    return this.direction || 'row';
+  }
+  @HostBinding('style.justify-content') get justifyContent() {
+    return this.justify || 'flex-start';
+  }
+  @HostBinding('style.align-items') get alignItems() {
+    return this.align || 'flex-start';
+  }
+  @HostBinding('style.width') get widthVal() {
+    return this.width;
+  }
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  styles() {
-    if (!this.align) {
-      return {};
-    }
-    return {
-      'align-items': this.align
-    };
   }
 }

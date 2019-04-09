fc59df66a61739c0bfae312670c2680a0ae1d55b
Tolsee <tolsee3@gmail.com>
2019-04-09 22:02:23 +0545

    Add all components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
+  @Input() nav: string;
+  @Output() changeNav = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Todo
+  // Better to make it work with other platform than browser
+  scroll(id: string) {
+    // const element = document.getElementById(id) as HTMLElement;
+    // element.scrollIntoView({ behavior: 'smooth',  block: 'start'});
+    this.changeNav.emit(id);
+  }
+
+  active(isNav) {
+    console.log('IS nav: ', isNav, this.nav);
+    return isNav === this.nav;
+  }
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,103 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, transition, style, animate, AnimateTimings } from '@angular/animations';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.sass']
+  styleUrls: ['./layout.component.sass'],
+  animations: [
+    trigger('page', [
+      transition('void => *', [
+        style({ opacity: '0' }),
+        animate('400ms ease-in')
+      ]),
+      transition('* => void', [
+        style({ opacity: '1' }),
+        animate('100ms ease-out')
+      ])
+    ])
+  ]
 })
 export class LayoutComponent implements OnInit {
+  currentNav = 'home';
+  totalNavs: string[] = [
+    'home',
+    'meetup',
+    'aboutus'
+  ];
+  currentPosition = 0;
+  pending = false;
+  touch = {
+    startY: 0
+  };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    document.addEventListener('wheel', this.scroll.bind(this), true);
+    document.addEventListener('touchstart', this.touchstart.bind(this), true);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('wheel', this.scroll.bind(this), true);
+    document.addEventListener('touchstart', this.touchstart.bind(this), true);
+  }
+
+  changeNav(nav: string) {
+    this.currentNav = nav;
+  }
+
+  nextNav() {
+    const currentIndex = this.totalNavs.indexOf(this.currentNav);
+    const { length } = this.totalNavs;
+    return this.totalNavs[(currentIndex + 1) % length];
+  }
+
+  prevNav() {
+    const currentIndex = this.totalNavs.indexOf(this.currentNav);
+    const { length } = this.totalNavs;
+    return this.totalNavs[currentIndex ? (currentIndex - 1) : length - 1];
+  }
+
+  handleScroll(deltaY) {
+    if (Math.abs(deltaY) < 30) { return; }
+    this.pending = true;
+
+    if (deltaY > 0) {
+      const next = this.nextNav();
+      this.changeNav(next);
+    } else if (deltaY < 0) {
+      const next = this.prevNav();
+      this.changeNav(next);
+    }
+
+    // Reduce the flicker effect
+    setTimeout(() => {
+      this.pending = false;
+    }, 400);
+  }
+
+  scroll(event) {
+    if (this.pending) { return; }
+    this.handleScroll(event.deltaY);
+  }
+
+  touchstart(event) {
+    if (this.pending) { return; }
+    const touches = event.touches;
+    if (touches && touches.length) {
+      this.touch.startY = touches[0].pageY;
+      document.addEventListener('touchmove', this.touchmove.bind(this));
+    }
+  }
+
+  touchmove(event) {
+    const touches = event.touches;
+    if (touches && touches.length) {
+      const deltaY = this.touch.startY - touches[0].pageY;
+      this.handleScroll(deltaY);
+      document.removeEventListener('touchmove', this.touchmove);
+    }
+  }
 }
+
